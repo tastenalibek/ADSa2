f5ab1322aa812478d931260749924f434fb41a8d
231227 <231227@astanait.edu.kz>
2024-04-21 22:19:51 +0500

    4 commit

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,103 +1,219 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class MyArrayList<T> implements MyList<T> {
-    private Object[] array;
-    private int size;
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
 
+    private Object[] elements; // Stores the elements of the array list
+    private int count = 0; // Tracks the number of elements stored
+    private static final int INITIAL_CAPACITY = 10; // Default capacity for the array list
 
-    @Override
-    public void add(T item) {
-
+    // Constructor for initializing the list with a specific capacity
+    public MyArrayList(int capacity) {
+        if (capacity < 0)
+            throw new IllegalArgumentException("Capacity must not be negative: " + capacity);
+        elements = new Object[capacity];
     }
 
-    @Override
-    public void set(int index, T item) {
-
+    // Default constructor for the list
+    public MyArrayList() {
+        this(INITIAL_CAPACITY);
     }
 
-    @Override
-    public void add(int index, T item) {
-
+    // Method to ensure there's enough space to add new elements
+    private void expandCapacity() {
+        Object[] largerArray = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, largerArray, 0, elements.length);
+        elements = largerArray;
     }
 
-    @Override
-    public void addFirst(T item) {
-
+    // Validates if the index is within the bounds of the array list
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= count) {
+            throw new IndexOutOfBoundsException("Index out of bounds: " + index);
+        }
     }
 
+    // Adds an element to the end of the list
     @Override
-    public void addLast(T item) {
-
+    public void add(T element) {
+        if (count == elements.length) {
+            expandCapacity();
+        }
+        elements[count++] = element;
     }
 
+    // Replaces the element at the given index with the provided element
+    @Override
+    public void set(int index, T element) {
+        rangeCheck(index);
+        elements[index] = element;
+    }
+
+    // Inserts an element at the given index
+    @Override
+    public void add(int index, T element) {
+        rangeCheck(index);
+        if (count == elements.length) {
+            expandCapacity();
+        }
+        System.arraycopy(elements, index, elements, index + 1, count - index);
+        elements[index] = element;
+        count++;
+    }
+
+    // Inserts an element at the start of the list
+    @Override
+    public void addFirst(T element) {
+        add(0, element);
+    }
+
+    // Appends an element to the end of the list (synonymous with add method)
+    @Override
+    public void addLast(T element) {
+        add(element);
+    }
+
+    // Retrieves the element at a specific index
     @Override
     public T get(int index) {
-        return null;
+        rangeCheck(index);
+        return (T) elements[index];
     }
 
+    // Retrieves the first element of the list
     @Override
     public T getFirst() {
-        return null;
+        if(count == 0){
+            throw new NoSuchElementException("List is empty.");
+        }
+        return (T) elements[0];
     }
 
+    // Retrieves the last element of the list
     @Override
     public T getLast() {
-        return null;
+        if(count == 0) {
+            throw new NoSuchElementException("List is empty.");
+        }
+        return (T) elements[count - 1];
     }
 
+    // Removes the element at a specific index
     @Override
     public void remove(int index) {
-
+        rangeCheck(index);
+        int elementsToMove = count - index - 1;
+        if (elementsToMove > 0) {
+            System.arraycopy(elements, index + 1, elements, index, elementsToMove);
+        }
+        elements[--count] = null;
     }
 
+    // Removes the first element of the list
     @Override
     public void removeFirst() {
-
+        remove(0);
     }
 
+    // Removes the last element of the list
     @Override
     public void removeLast() {
-
+        remove(count - 1);
     }
 
+    // Sorts the list in ascending order using the Comparable interface
     @Override
     public void sort() {
-
+        for (int i = 0; i < count; i++) {
+            for (int j = i + 1; j < count-1-i; j++) {
+                if (((Comparable)elements[j]).compareTo(elements[j+1]) > 0) {
+                    Object temp = elements[j+1];
+                    elements[j+1] = elements[j];
+                    elements[j] = temp;
+                }
+            }
+        }
     }
 
+    // Finds the index of the first occurrence of a specific element
     @Override
     public int indexOf(Object object) {
-        return 0;
+        if (object == null) {
+            for (int i = 0; i < count; i++)
+                if (elements[i] == null)
+                    return i;
+        } else {
+            for (int i = 0; i < count; i++)
+                if (object.equals(elements[i]))
+                    return i;
+        }
+        return -1;
     }
 
+    // Finds the index of the last occurrence of a specific element
     @Override
     public int lastIndexOf(Object object) {
-        return 0;
+        if (object == null) {
+            for (int i = count - 1; i >= 0; i--)
+                if (elements[i] == null)
+                    return i;
+        } else {
+            for (int i = count - 1; i >= 0; i--)
+                if (object.equals(elements[i]))
+                    return i;
+        }
+        return -1;
     }
 
+    // Checks if a specific element is in the list
     @Override
     public boolean exists(Object object) {
-        return false;
+        return indexOf(object) >= 0;
     }
 
+    // Converts the list into an array
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        return java.util.Arrays.copyOf(elements, count);
     }
 
+    // Clears all elements from the list
     @Override
     public void clear() {
-
+        for (int i = 0; i < count; i++) {
+            elements[i] = null;
+        }
+        count = 0;
     }
 
+    // Returns the number of elements in the list
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
+    // Provides an iterator for the list
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<T>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < count;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) elements[currentIndex++];
+            }
+        };
     }
 }
+
+
+
 
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,18 +1,257 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class MyLinkedList<T> implements MyList<T> {
-    private Node<T> head;
-    private Node<T> tail;
-    private int size;
+public class MyLinkedList<T> implements MyList<T>{
 
-    private static class Node<T> {
-        T data;
-        Node<T> next;
-        Node<T> prev;
+    class MyNode<E> {
+        E element; // The value stored within the node
+        MyNode next; // Reference to the next node in the sequence
+        MyNode prev; // Reference to the previous node in the sequence
+
+        public MyNode(E element) {
+            this.element = element;
+            this.next = null;
+            this.prev = null;
+        } // Node constructor for storing a value
+
+        public MyNode(E element, MyNode next, MyNode prev) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        } // Node constructor for linking to other nodes
+
+        public MyNode() {
+            this.element = null;
+            this.next = null;
+            this.prev = null;
+        } // Default node constructor
+    } // Node class
+
+    private MyNode head; // Points to the first node in the list
+    private MyNode tail; // Points to the last node in the list
+    private int count; // Tracks the total number of nodes in the list
 
 
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        count = 0;
+    } // Constructor with no parameter
+
+
+
+    private void checkCap(int index) {
+        if(index < 0|| index >= count)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + count);
     }
 
+    @Override
+    public void add(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        count++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        checkCap(index);
+        MyNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.element = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        checkCap(index);
+        MyNode newNode = new MyNode(item);
+        if (index == 0) {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        else if (index == count) {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        else {
+            MyNode current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+        count++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        checkCap(index);
+        MyNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return (T) current.element;
+    } // Returns the element at the specified position in this list
+
+    @Override
+    public T getFirst() {
+        return (T) head.element;
+    } // Returns the first element in this list
+
+    @Override
+    public T getLast() {
+        return (T) tail.element;
+    }// Returns the last element in this list
+
+    @Override
+    public void remove(int index) {
+        checkCap(index);
+        if (index == 0) {
+            head = head.next;
+            head.prev = null;
+        }
+        else if (index == count - 1) {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        else {
+            MyNode current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        count--;
+    } // Removes the element at the specified position in this list
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    } // Removes the first element from this list
+
+    @Override
+    public void removeLast() {
+        remove(count - 1);
+    } // Removes the last element from this list
+
+    @Override
+    public void sort() {
+        for (MyNode i = head; i != null; i = i.next) {
+            for (MyNode j = i.next; j != null; j = j.next) {
+                if (((Comparable)j.element).compareTo(i.element) < 0) {
+                    Object current = i.element;
+                    i.element = j.element;
+                    j.element = current;
+                }
+            }
+        }
+    } // Sorts the elements of this list in ascending order
+
+
+
+
+    @Override
+    public int indexOf(Object object) {
+        MyNode current = head;
+        for (int i = 0; i < count; i++) {
+            if (current.element.equals(object)) {
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
+    } // Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the element
+
+    @Override
+    public int lastIndexOf(Object object) {
+        MyNode current = tail;
+        for (int i = count - 1; i >= 0; i--) {
+            if (current.element.equals(object)) {
+                return i;
+            }
+            current = current.prev;
+        }
+        return -1;
+    } // Returns the index of the last occurrence of the specified element in this list, or -1 if this list does not contain the element
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    } // Returns true if this list contains the specified element
+
+    @Override
+    public Object[] toArray() {
+        Object[] arr = new Object[count];
+        MyNode current = head;
+        for (int i = 0; i < count; i++) {
+            arr[i] = current.element;
+            current = current.next;
+        }
+        return arr;
+    } // Returns an array containing all of the elements in this list in proper sequence
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        count = 0;
+    } // Removes all of the elements from this list
+
+    @Override
+    public int size() {
+        return count;
+    } // Returns the number of elements in this list
+
+    @Override
+    public Iterator iterator() {
+        return new MyIterator();
+    } // Returns an iterator over the elements in this list in proper sequence
+
+    public class MyIterator implements Iterator<T> {
+        private MyNode current = head;
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < count;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext() != true) {
+                throw new NoSuchElementException();
+            }
+            T element = (T) current.element;
+            current = current.next;
+            index++;
+            return element;
+        }
+    }
 
 }
 
